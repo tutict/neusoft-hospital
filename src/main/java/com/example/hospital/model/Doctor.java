d91ffd18e8322789345de9ee772faa99e8ac6051
tutict <tutict@163.com>
2023-10-18 15:52:36 +0800

    5-commit

--- src/main/java/com/example/hospital/model/Doctor.java
+++ src/main/java/com/example/hospital/model/Doctor.java
@@ -1,4 +1,33 @@
 package com.example.hospital.model;
 
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role")
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Getter
+    private String name;
+    @Getter
+    private String specialization;
+    public Doctor() {
+    }
+    public Doctor(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSpecialization(String specialization){
+        this.specialization = specialization;
+    }
 }
